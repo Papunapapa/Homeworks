398c3ac0041c243df7fbd1edf32c9f55ec83115b
Papunapapa <papunapapashvili2@gmail.com>
2024-11-01 18:03:24 +0400

    first commit

---
+++ src/Main.java
@@ -0,0 +1,50 @@
+public class Main {
+    public static void main(String[] args) {
+
+        //1
+
+//          int sum = 0;
+//          for(int i = 10 ; i<=100 ; i+=5){
+//              sum+=1;
+ //             System.out.println(sum);
+
+
+        //2
+//         int number = 5432;
+//         int lenght = 0;
+
+//         do {
+//             number /= 10;
+//             lenght++;
+ //        }while(number > 0);
+ //        System.out.println("ricxvi" + lenght + "nishnaa");
+
+
+        //3
+//           int number = 29;
+
+ //          boolean isPrime = true;
+
+//           for(int i = 2; i < number; i++) {
+        //if (number % i == 0) {
+         //          isPrime = false;
+        //           break;}
+          //         if(number == 1){
+         //              System.out.println("ertis tolia");
+
+         //             else if(isPrime)
+
+
+        //4
+        //   int a = 120;
+        //   int b = 180;
+       //    int usg = 0;
+        //       for(int i = Math.min(a,b); i >= 1;i-- ){
+          //         if (a % i == 0 && b % i == 0){
+           //            usg += i;
+           //            break;
+                   }
+             //      System.out.println(usg);
+
+    {
+
